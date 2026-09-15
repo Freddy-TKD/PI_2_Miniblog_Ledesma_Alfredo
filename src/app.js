0c67acfb3d5b7033917af54216d7d4c9301ee9f5
Freddy Ledesma <freddyledesma.al@gmail.com>
2026-09-15 16:14:42 -0300

    se armeó y se subio el archivo app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express"
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json({ message: "MiniBlog API funcionando" })
+})
+
+export default app
